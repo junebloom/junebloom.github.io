124e662c639a398996f2bb6963800b1915f6267a
June Bloom <june.a.bloom@gmail.com>
2020-10-23 12:51:37 -0700

    Add scroll behavior for header background

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,34 @@
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Header = ({ siteTitle }) => (
-  <header className="absolute w-full flex justify-between p-12">
-    <Link to="/" className="font-black">
-      j
-    </Link>
-    <Link to="#">menu</Link>
-  </header>
-);
+const Header = ({ siteTitle }) => {
+  const [scrolled, setScrolled] = useState(false);
+
+  function handleScroll() {
+    const { scrollTop } = document.scrollingElement;
+    if (scrolled !== scrollTop > 0) setScrolled(scrollTop > 0);
+  }
+
+  useEffect(() => {
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  let className = `fixed z-10 w-full flex justify-between p-8 lg:bg-transparent`;
+  if (scrolled) className += ` bg-white`;
+
+  return (
+    <header className={className}>
+      <Link to="/" className="font-black">
+        j
+      </Link>
+      <Link to="#">menu</Link>
+    </header>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
