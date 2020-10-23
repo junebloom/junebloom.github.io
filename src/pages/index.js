0929c91110a5255f7639c9bc2dcf3e0a0bcaa242
June Bloom <june.a.bloom@gmail.com>
2020-10-22 17:44:24 -0700

    Add wireframe styles for index content

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,35 +11,63 @@ const IndexPage = () => (
       {/* name container */}
       <div className="flex flex-col">
         {/* title */}
-        <div className="text-6xl font-black">Juniper</div>
+        <div className="text-6xl font-black">juniper</div>
         {/* subtitle */}
         <code className="text-right">software.engineer();</code>
       </div>
     </section>
-    {/* Hire me */}
-    <section>
-      <h2>Looking for help? Add me to your project.</h2>
-      <code>{`import { Juniper } from "june.a.bloom@gmail.com";`}</code>
-    </section>
-    {/* Blog */}
-    <section>
-      <h2>Read my documentation.</h2>
-      <ul>
-        <li>Is This a Blog Post? Yes This Is a Blog Post.</li>
-        <li>Is This a Blog Post? Yes This Is a Blog Post.</li>
-        <li>Is This a Blog Post? Yes This Is a Blog Post.</li>
-        <li>See more</li>
-      </ul>
-    </section>
-    {/* Projects */}
-    <section>
-      <h2>Explore my source code.</h2>
-      <ul>
-        <li>Project</li>
-        <li>Project</li>
-        <li>Project</li>
-      </ul>
-    </section>
+
+    {/* Content */}
+    <div className="max-w-screen-md mx-auto px-4 space-y-24">
+      {/* Hire me */}
+      <section className="flex flex-col space-y-2">
+        <h2 className="font-bold p-2">
+          Looking for help? Add me to your project.
+        </h2>
+        <code className="font-bold p-2 overflow-auto whitespace-no-wrap bg-indigo-100">
+          {`import { Juniper } from "june.a.bloom@gmail.com";`}
+        </code>
+      </section>
+
+      {/* Blog */}
+      <section className="flex flex-col space-y-2">
+        <h2 className="font-bold p-2">Read my documentation.</h2>
+        <ul className="list-inside list-disc font-bold ml-8 underline">
+          <li>Is This a Blog Post? Yes This Is a Blog Post.</li>
+          <li>Is This a Blog Post? Yes This Is a Blog Post.</li>
+          <li>Is This a Blog Post? Yes This Is a Blog Post.</li>
+          <li>See more</li>
+        </ul>
+      </section>
+
+      {/* Projects */}
+      <section className="flex flex-col space-y-2">
+        <h2 className="font-bold p-2">Explore my source code.</h2>
+        <ul className="font-bold flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2">
+          <li className="relative flex md:flex-col items-center md:justify-center flex-grow md:h-56 p-2 bg-indigo-100">
+            <span className="m-2 w-16 h-16 border-4 rounded-full border-indigo-600"></span>
+            <div className="md:absolute md:bottom-0 flex flex-grow md:flex-col md:items-center justify-between">
+              <div className="m-2 md:m-0">Project</div>
+              <div className="m-2 md:m-0 md:mb-2">This is a project.</div>
+            </div>
+          </li>
+          <li className="relative flex md:flex-col items-center md:justify-center flex-grow md:h-56 p-2 bg-indigo-100">
+            <span className="m-2 w-16 h-16 border-4 rounded-full border-indigo-600"></span>
+            <div className="md:absolute md:bottom-0 flex flex-grow md:flex-col md:items-center justify-between">
+              <div className="m-2 md:m-0">Project</div>
+              <div className="m-2 md:m-0 md:mb-2">This is a project.</div>
+            </div>
+          </li>
+          <li className="relative flex md:flex-col items-center md:justify-center flex-grow md:h-56 p-2 bg-indigo-100">
+            <span className="m-2 w-16 h-16 border-4 rounded-full border-indigo-600"></span>
+            <div className="md:absolute md:bottom-0 flex flex-grow md:flex-col md:items-center justify-between">
+              <div className="m-2 md:m-0">Project</div>
+              <div className="m-2 md:m-0 md:mb-2">This is a project.</div>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </div>
   </Layout>
 );
 
