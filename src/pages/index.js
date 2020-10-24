5a73052de688798f863de121a42b7de8390a2e11
June Bloom <june.a.bloom@gmail.com>
2020-10-23 21:34:11 -0700

    Tweak text styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,9 +28,14 @@ const IndexPage = () => (
       {/* name container */}
       <div className="z-10 flex flex-col">
         {/* title */}
-        <div className="text-6xl font-black text-indigo-500">juniper</div>
+        <div
+          className="font-black text-indigo-500"
+          style={{ "font-size": "6rem" }}
+        >
+          juniper
+        </div>
         {/* subtitle */}
-        <code className="text-right font-bold text-indigo-600">
+        <code className="text-right font-mono font-bold text-indigo-600">
           software.engineer();
         </code>
       </div>
@@ -43,13 +48,23 @@ const IndexPage = () => (
         <h2 className="font-bold text-indigo-900 p-2">
           Looking for help? Add me to your project.
         </h2>
-        <code className="font-bold text-indigo-900 p-2 overflow-auto whitespace-no-wrap bg-indigo-100">
+        <code className="font-mono font-bold text-indigo-900 p-2 overflow-auto whitespace-no-wrap bg-indigo-100">
           <span className="text-indigo-600">import</span>
           {" { "}
           <span className="text-indigo-500">Juniper</span>
           {" } "}
           <span className="text-indigo-600">from</span>{" "}
-          <span className="text-red-400">"june.a.bloom@gmail.com"</span>;
+          <span className="text-red-400">
+            "
+            <a
+              href="mailto:june.a.bloom@gmail.com"
+              className="hover:text-red-500 hover:underline"
+            >
+              june.a.bloom@gmail.com
+            </a>
+            "
+          </span>
+          ;
         </code>
       </section>
 
@@ -73,24 +88,30 @@ const IndexPage = () => (
         </h2>
         <ul className="font-bold flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2">
           <li className="relative flex md:flex-col items-center md:justify-center flex-grow md:h-56 p-2 bg-indigo-100">
-            <span className="m-2 w-16 h-16 border-4 rounded-full border-indigo-600"></span>
-            <div className="md:absolute md:bottom-0 flex flex-grow md:flex-col md:items-center justify-between text-indigo-600">
-              <div className="m-2 md:m-0">Project</div>
-              <div className="m-2 md:m-0 md:mb-2">This is a project.</div>
+            <span className="m-2 w-16 h-16 border-4 rounded-full border-indigo-500"></span>
+            <div className="md:absolute md:bottom-0 flex flex-grow md:flex-col md:items-center justify-between md:text-center leading-none text-indigo-500">
+              <div className="text-lg m-2 md:m-0">Scrawl</div>
+              <div className="font-medium m-2 md:m-0 md:mb-6">
+                World-building Tool
+              </div>
             </div>
           </li>
           <li className="relative flex md:flex-col items-center md:justify-center flex-grow md:h-56 p-2 bg-indigo-100">
-            <span className="m-2 w-16 h-16 border-4 rounded-full border-purple-500"></span>
-            <div className="md:absolute md:bottom-0 flex flex-grow md:flex-col md:items-center justify-between text-purple-500">
-              <div className="m-2 md:m-0">Project</div>
-              <div className="m-2 md:m-0 md:mb-2">This is a project.</div>
+            <span className="m-2 w-16 h-16 border-4 rounded-full border-purple-600"></span>
+            <div className="md:absolute md:bottom-0 flex flex-grow md:flex-col md:items-center justify-between leading-none text-purple-600">
+              <div className="text-lg m-2 md:m-0">Practical</div>
+              <div className="font-medium m-2 md:m-0 md:mb-6">
+                Audio Recorder
+              </div>
             </div>
           </li>
           <li className="relative flex md:flex-col items-center md:justify-center flex-grow md:h-56 p-2 bg-indigo-100">
             <span className="m-2 w-16 h-16 border-4 rounded-full border-red-400"></span>
-            <div className="md:absolute md:bottom-0 flex flex-grow md:flex-col md:items-center justify-between text-red-400">
-              <div className="m-2 md:m-0">Project</div>
-              <div className="m-2 md:m-0 md:mb-2">This is a project.</div>
+            <div className="md:absolute md:bottom-0 flex flex-grow md:flex-col md:items-center justify-between leading-none text-red-400">
+              <div className="text-lg m-2 md:m-0">Fae</div>
+              <div className="font-medium m-2 md:m-0 md:mb-6">
+                Game Framework
+              </div>
             </div>
           </li>
         </ul>
