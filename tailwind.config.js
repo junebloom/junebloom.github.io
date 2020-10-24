56202b5e2813cee417ce09b9f77e0bb1689ef02e
June Bloom <june.a.bloom@gmail.com>
2020-10-23 19:33:04 -0700

    Add custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        indigo: {
+          100: "#EFEFFE",
+          500: "#6F75F6",
+          600: "#595EE6",
+          900: "#191EA2",
+        },
+        red: {
+          400: "#FF79A1",
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
