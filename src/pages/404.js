0f3d41ed8685ea183b261b62b0597be4bcadc2d7
June Bloom <june.a.bloom@gmail.com>
2020-10-26 13:17:47 -0700

    Add proper 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,14 @@ import SEO from "../components/seo";
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404 Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn't exist... the sadness.</p>
+    <SEO title="404 - Not Found" />
+    <div className="flex-grow flex flex-col items-center justify-center leading-none">
+      <h1 className="text-6xl font-black text-indigo-500">404</h1>
+      <h2 className="text-2xl text-indigo-400">Not Found</h2>
+      <p className="font-bold text-indigo-400 mt-2">
+        That page doesn't seem to exist.
+      </p>
+    </div>
   </Layout>
 );
 
