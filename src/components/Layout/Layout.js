754e3a99a2e7c53db4ca94e8f9d6bae94b7d0b9c
June Bloom <june.a.bloom@gmail.com>
2020-10-28 17:51:24 -0700

    Make query anonymous

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,7 +7,7 @@ import "./Layout.css";
 
 export function Layout({ children }) {
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
+    query {
       site {
         siteMetadata {
           title
