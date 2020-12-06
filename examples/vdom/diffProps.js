f12fb57a3d6f1c3a1c5a14586a3b4fa3433673c5
June Bloom <june.a.bloom@gmail.com>
2020-12-05 21:33:26 -0800

    Improve variable names

--- examples/vdom/diffProps.js
+++ examples/vdom/diffProps.js
@@ -1,26 +1,26 @@
 // Shallowly compare the props of VDOM nodes `a` and `b`,
 // returning a map of changes, or null if there were no changes.
 export function diffProps(a, b) {
-  const propsA = a[1];
-  const propsB = b[1];
+  const oldProps = a[1];
+  const newProps = b[1];
 
   const diff = {};
   let changed = false;
 
   // Iterate over the props of each element and compare them, starting with a.
-  for (const prop in propsA) {
+  for (const prop in oldProps) {
     // This will catch any props that were updated or removed between a and b.
-    if (propsA[prop] !== propsB[prop]) {
-      diff[prop] = propsB[prop];
+    if (oldProps[prop] !== newProps[prop]) {
+      diff[prop] = newProps[prop];
       changed = true;
     }
   }
 
   // Now iterate over b's props.
-  for (const prop in propsB) {
+  for (const prop in newProps) {
     // This catches any new props of b that weren't present on a.
-    if (propsA[prop] === undefined) {
-      diff[prop] = propsB[prop];
+    if (oldProps[prop] === undefined) {
+      diff[prop] = newProps[prop];
       changed = true;
     }
   }
